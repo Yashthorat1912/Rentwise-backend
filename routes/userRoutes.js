3b3cd7199d1cad6da90c1ff9d990490a5eabef4e
Yash Thorat <yashthorat1912@gmail.com>
2026-04-20 17:15:24 +0530

    fixed user bug

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,24 @@
 const express = require("express");
-
 const router = express.Router();
-
+const User = require("../models/User");
+const authMiddleware = require("../middleware/authMiddleware");
 const { getTenants } = require("../controllers/userController");
 
 router.get("/tenants", getTenants);
+
+// ✅ ADD THIS
+router.put("/save-fcm-token", authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.user.id,
+      { fcmToken: req.body.token },
+      { new: true },
+    );
+
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
 
 module.exports = router;
